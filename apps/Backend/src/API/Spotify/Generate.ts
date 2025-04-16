1ae6ba4939d08d33fe0d8dc8f7ea305c86ca4c91
shivamsaklani <shivasaklani1@gmail.com>
2025-04-16 19:22:48 +0530

    removed-files

---
+++ apps/Backend/src/API/Spotify/Generate.ts
@@ -0,0 +1,12 @@
+var generateRandomString = function (len:number) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < len; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  export default generateRandomString;
+  
--- apps/Backend/src/API/Spotify/spotify.ts
+++ apps/Backend/src/API/Spotify/spotify.ts
@@ -1,16 +1,27 @@
 const {Router }=require("express");
 import dotenv from 'dotenv';
 import { Request, Response } from 'express';
+import generateRandomString from "./Generate"
 dotenv.config();
 const router = Router();
-const {ClientID}= require("./configure");
-console.log(ClientID);
-router.get("/get",(req:Request,res:Response)=>{
-   
-
-    res.json({
-        mesg:process.env.ClientID
+const ClientID=process.env.ClientID;
+const ClientKey=process.env.ClientKey;
+const RedirectURL=process.env.RedirectURL;
+var scope = "streaming \
+               user-read-email \
+               user-read-private";
+router.get("/authorize",(req:Request,res:Response)=>{
+    var state= generateRandomString(16);
+    console.log(state);
+    var auth_query_parameters = new URLSearchParams({
+        response_type:"code",
+        client_id:ClientID,
+        scope:scope,
+        redirect_uri:RedirectURL,
+        state:state
     });
+
+    res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
 
 });
 
