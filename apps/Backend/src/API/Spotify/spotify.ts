983149277953fda2628f06ec2575c9d374e1b991
shivamsaklani <shivasaklani1@gmail.com>
2025-04-17 10:00:26 +0530

    auth-logic

--- apps/Backend/src/API/Spotify/spotify.ts
+++ apps/Backend/src/API/Spotify/spotify.ts
@@ -1,27 +1,60 @@
-const {Router }=require("express");
+const { Router } = require("express");
 import dotenv from 'dotenv';
 import { Request, Response } from 'express';
-import generateRandomString from "./Generate"
+import generateRandomString from "./Generate";
+import axios from 'axios';
+import qs from "qs";
 dotenv.config();
 const router = Router();
-const ClientID=process.env.ClientID;
-const ClientKey=process.env.ClientKey;
-const RedirectURL=process.env.RedirectURL;
+const ClientID = process.env.ClientID;
+const ClientKey = process.env.ClientKey;
+const RedirectURL = process.env.RedirectURL;
 var scope = "streaming \
                user-read-email \
                user-read-private";
-router.get("/authorize",(req:Request,res:Response)=>{
-    var state= generateRandomString(16);
-    console.log(state);
+router.get("/authorize", (req: Request, res: Response) => {
+    var state = generateRandomString(16);
     var auth_query_parameters = new URLSearchParams({
-        response_type:"code",
-        client_id:ClientID,
-        scope:scope,
-        redirect_uri:RedirectURL,
-        state:state
+        response_type: "code",
+        client_id: ClientID,
+        scope: scope,
+        redirect_uri: RedirectURL,
+        state: state
     });
 
     res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+
+});
+
+router.get('/auth/callback', async (req:Request, res:Response) => {
+    const code = req.query.code;
+     console.log(code);
+    const authOptions = {
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: qs.stringify({
+        code: code,
+        redirect_uri: RedirectURL,
+        grant_type: 'authorization_code'
+      }),
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(ClientID+ ':' + ClientKey).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+  
+    try {
+      const response = await axios(authOptions);
+      const access_token = response.data.access_token;
+      res.redirect('/');
+    } catch (error) {
+      console.error('Error getting token:', error);
+      res.status(500).send('Authentication failed');
+    }
+  });
+  
+
+router.get("/auth", (req: Request, res: Response) => {
 
 });
 
--- apps/frontend/src/app/AuthPage/Auth.tsx
+++ apps/frontend/src/app/AuthPage/Auth.tsx
@@ -1,35 +1,49 @@
 "use client";
+import axios from "axios";
 import { Button } from "../components/Button";
 import { SpotifyIcon } from "../components/Spotify";
 import MusicIcon from "@/app/assets/icons/Music.svg";
-export const AuthPage =()=>{
 
-    return(
-        <div className="flex bg-radial-[at_50%_75%] from-primary to-secondary/30 justify-center items-center bg-primary min-h-screen px-4">
-        <div className="w-full flex flex-col justify-between  space-y-5 max-w-md bg-black/70 text-white rounded-xl backdrop-blur-xl backdrop-brightness-150 shadow-lg p-8">
-      
+const Authorization = async () => {
+  // const clientId= process.env.NEXT_PUBLIC_ClientID;
+  // const redirectUrl = process.env.NEXT_PUBLIC_RedirectURL;
+  // const apiUrl= "https://accounts.spotify.com/authorize/";
+  // var scope = ["streaming",
+  // "user-read-email",
+  // "user-read-private"];
 
-         <div className="flex font-arial flex-col items-center">
+  // window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")}&response_type=token&show_daialog=true`;
+  window.location.href = "/api/v1/spotify/authorize";
+
+}
+export const AuthPage = () => {
+
+  return (
+    <div className="flex bg-radial-[at_50%_75%] from-primary to-secondary/30 justify-center items-center bg-primary min-h-screen px-4">
+      <div className="w-full flex flex-col justify-between  space-y-5 max-w-md bg-black/70 text-white rounded-xl backdrop-blur-xl backdrop-brightness-150 shadow-lg p-8">
+
+
+        <div className="flex font-arial flex-col items-center">
           <div className=" gap-2 flex justify-center items-center">
-            <MusicIcon className="sm:size-10 size-5"/>
+            <MusicIcon className="sm:size-10 size-5" />
             <span className="sm:text-4xl  ">Listen your Music</span>
-            <MusicIcon className="sm:size-10 size-5"/>
-            </div>
+            <MusicIcon className="sm:size-10 size-5" />
+          </div>
           <span className="text-sm sm:text-lg ">According to your mood</span>
-         </div>
-         <hr className="w-full h-2 text-secondary"/>
-        
-         <Button onclick={()=>{} } size="lg" variant="secondary">
+        </div>
+        <hr className="w-full h-2 text-secondary" />
+
+        <Button onclick={Authorization} size="lg" variant="secondary">
           <div className="flex flex-col cursor-pointer justify-center items-center">
-          <SpotifyIcon className="sm:size-20 size-10"/>
-          <h1>
-          Login to Spotify 
+            <SpotifyIcon className="sm:size-20 size-10" />
+            <h1>
+              Login to Spotify
             </h1>
           </div>
-          
-         
-          </Button>
-          </div>  
-        </div>
-    )
+
+
+        </Button>
+      </div>
+    </div>
+  )
 }
